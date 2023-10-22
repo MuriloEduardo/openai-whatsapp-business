5cd1633beefa664abac94e492c2023c4ea1ee0b2
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-21 23:03:00 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const pubsub = require("./utils/pubsub");
+
+const subscription = pubsub.subscription('my-subscription');
+
+const getOpenAIResponse = async (message) => {
+    try {
+        const aiResponse = await fetch(`${OPENAI_API_URL}/conversations`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message }),
+        })
+
+        const aiResponseJson = await aiResponse.json()
+        const aiChoices = aiResponseJson.choices
+
+        return aiChoices
+    } catch (error) {
+        console.error('getOpenAIResponse error', error)
+    }
+}
+
+const sendMessageToWhatsApp = async (message) => {
+    try {
+        const whatsappResponse = await fetch('https://api.whatsapp.com/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                phone: process.env.WHATSAPP_PHONE_NUMBER,
+                text: message
+            })
+        });
+
+        console.log(`Message sent: ${message}`, whatsappResponse);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+subscription.on('message', async (message) => {
+    try {
+        const data = JSON.parse(message.data.toString());
+        const text = data.text;
+
+        const aiChoices = await getOpenAIResponse(text)
+
+        for (const choice of aiChoices) {
+            const aiResponse = choice.message.content
+
+            const sended = await sendMessageToWhatsApp(aiResponse)
+
+            console.log('sendMessageToWhatsApp', sended);
+        }
+    } catch (error) {
+        console.error('Error onmessage subscription', error);
+    }
+
+    message.ack();
+});
---
+++ utils/env.js
@@ -0,0 +1,3 @@
+require('dotenv').config()
+
+exports.OPENAI_API_URL = process.env.OPENAI_API_URL
