c1d6042c95e99e57ef972814d6b1e5333cb525c0
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-22 02:23:38 -0300

    Remove pubsub

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const express = require('express')
 const { PORT } = require("./utils/env")
-const { pubsub } = require("./utils/pubsub")
 const { getOpenAIResponse } = require("./services/openai")
 const { getExtractedInfos, sendMessageToWhatsApp } = require("./services/whatsapp-business")
 
 const app = express()
 
-const subscription = pubsub.subscription('whatsapp-business-messages-sub')
+const processWhatsAppBusinessMessages = async (data) => {
+    const { text, from, to } = await getExtractedInfos(data)
 
-subscription.on('message', async (message) => {
-    try {
-        const data = JSON.parse(message.data.toString())
-
-        const { text, from, to } = await getExtractedInfos(data)
-
-        if (!text || !text.length) {
-            console.error('No text found')
-            message.ack()
-            return
-        }
-
-        const aiChoices = await getOpenAIResponse(text)
-
-        for (const choice of aiChoices) {
-            const aiResponse = choice.message.content
-
-            await sendMessageToWhatsApp(aiResponse, from, to)
-        }
-
+    if (!text || !text.length) {
+        console.error('No text found')
         message.ack()
-    } catch (error) {
-        console.error('whatsapp-business-messages-sub error', error)
+        return
+    }
 
-        message.nack()
+    const aiChoices = await getOpenAIResponse(text)
+
+    for (const choice of aiChoices) {
+        const aiResponse = choice.message.content
+
+        await sendMessageToWhatsApp(aiResponse, from, to)
+    }
+}
+
+app.post('/receive-push', async (req, res) => {
+    try {
+        const data = req.body
+
+        console.log('receive-push', data);
+
+        processWhatsAppBusinessMessages(data)
+
+        res.sendStatus(200)
+    } catch (error) {
+        console.error('receive-push error', error)
+
+        res.sendStatus(500)
     }
 })
 
